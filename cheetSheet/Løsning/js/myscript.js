307d5bcdf3e73e960932f4431626c39013906a34
dominoeffekten <rikkedomino@gmail.com>
2021-09-21 14:06:36 +0200

    B

--- cheetSheet/Løsning/js/myscript.js
+++ cheetSheet/Løsning/js/myscript.js
@@ -5,12 +5,9 @@ document.getElementById("send").addEventListener("click", function() {
     let caption = document.getElementById("caption").value;
     let rows = parseInt(document.getElementById("rows").value);
     let cols = parseInt(document.getElementById("cols").value);
-    
     /*console.log(caption);*/
     //console.log(rows);
     //console.log(cols);
-   
-
     createTables(rows, cols, caption);
 
     document.getElementById("createListPlaceholder").style.display = "block";
@@ -23,24 +20,21 @@ document.getElementById("send").addEventListener("click", function() {
 
 function createTables(rows, cols, caption){
     let pTag = "<table>" + "<caption>" + caption + "</caption>";
-    
         for(var i = 0; i < rows; i++){ //table rows
+            
             pTag += "<tr>";
-
-            for(var j = 0; j < cols; j++){
+            for(var j = 0; j < rows; j++){
                 pTag += "<th>" + "</th>";
-            }
+            };
+            pTag += "</tr>" + "<tr>";
 
             for(var k = 0; k < cols; k++){
                 pTag += "<td>" + "</td>";
-            }
+            };
             pTag += "</tr>";
             
-        }
+        };
         pTag += "</table>";
 
-        console.log(pTag)
-        
-    
-    
+        console.log(pTag)  
 }
