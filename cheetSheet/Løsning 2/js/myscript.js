f56c7c0914eceaa241393408013c5302293bb640
dominoeffekten <rikkedomino@gmail.com>
2021-10-07 21:28:03 +0200

    update

---
+++ cheetSheet/Løsning 2/js/myscript.js
@@ -0,0 +1,74 @@
+console.log("Hello World");
+let st = "&lt;";
+let sl = "&gt;";
+
+/* create a a table */
+document.getElementById("createTableNow").addEventListener("click", function() {
+    document.getElementById("createTablePlaceholder").innerHTML = "";
+    let caption = document.getElementById("caption").value;
+    let rows = parseInt(document.getElementById("rows").value);
+    let cols = parseInt(document.getElementById("cols").value);
+    createTables(rows, cols, caption);
+    document.getElementById("createTablePlaceholder").style.display = "block";
+});
+    function createTables(rows, cols, caption){
+        let pTag = st+"table"+sl + "<br>" + st+"caption"+sl + caption + st+"/caption"+sl + "<br>";
+
+        pTag += st+"tr"+sl + "<br>"; //table rows heading start
+        for(var j = 0; j < cols; j++){
+            pTag += st+"th"+sl + st+"th"+sl + "<br>"; //table headings
+        };
+        pTag += "&lt;/tr&gt; <br>" //table row heading end
+
+        for(var i = 0; i < rows; i++){ //number of table rows
+            pTag += st+"tr"+sl + "<br>";
+            for(var k = 0; k < cols; k++){
+                pTag += st+"td"+sl + st+"/td"+sl + "<br>"; //table data
+            };
+            pTag += st+"/tr"+sl + "<br>";
+        };
+        pTag += st+"/table"+sl ;
+        document.getElementById("createTablePlaceholder").innerHTML = pTag;
+    }
+    //delete content
+    document.getElementById("deleteTable").addEventListener("click", function() {
+        document.getElementById("createListPlaceholder").innerHTML = "";
+        document.getElementById("caption").value = "";
+        document.getElementById("rows").value ="";
+        document.getElementById("cols").value ="";
+        document.getElementById("createTablePlaceholder").innerHTML = "";
+        document.getElementById("createListPlaceholder").style.display = "none";
+    });
+
+//Create lists
+document.getElementById("createListsNow").addEventListener("click", function() {
+    //document.getElementById("createListPlaceholder").innerHTML = "";
+    let ordredList = document.getElementById("ordredList");
+    let unordredList = document.getElementById("unordredList");
+    let listNumber = parseInt(document.getElementById("listNumber").value);
+    if(ordredList.value && ordredList.checked){ //if it is an ordred list 
+        console.log("hello1 ol");
+        let listType = "ol";
+        createList(listNumber, listType);
+    }
+    if(unordredList.value && unordredList.checked){ //if it is an unordred list 
+        console.log("hello1 ul");
+        let listType = "ul";
+        createList(listNumber, listType);
+    }
+});
+    function createList(listNumber, ListType){
+        let listTag = st + ListType + sl + "<br>"; //ul / ol
+        for(var i = 0; i < listNumber; i++){
+            listTag += st+"li"+sl + st+"/li"+sl +"<br>"; //lists
+        };
+        listTag += st + ListType + sl;
+        document.getElementById("createListPlaceholder").innerHTML = listTag;
+        document.getElementById("createListPlaceholder").style.display = "block";
+    }
+    //delete content
+    document.getElementById("deleteList").addEventListener("click", function() {
+        document.getElementById("listNumber").value = "";
+        document.getElementById("createListPlaceholder").innerHTML = "";
+        document.getElementById("createListPlaceholder").style.display = "none";
+    });
---
+++ cheetSheet/Løsning 2/js/sortOut.js
@@ -0,0 +1,33 @@
+'use strict';
+console.log("Hello You");
+
+const elements = document.querySelectorAll('article'); //all articles
+
+const all = document.querySelectorAll('button')[0]; //All butto
+const html = document.querySelectorAll('button')[1]; //HTML button
+const css = document.querySelectorAll('button')[2]; //CSS button
+const js = document.querySelectorAll('button')[3]; //JS button
+const seo = document.querySelectorAll('button')[4]; //SEO button
+const program = document.querySelectorAll('button')[5]; //program button
+let buttons = [all, html, css, js, seo, program] //insert them in an array
+
+buttons.forEach(button => { 
+    button.addEventListener('click', function() {
+        let selectedFilter = button.innerHTML.toLowerCase(); //get the value from the buttonsand make them lowercase
+        let itemsToHide = document.querySelectorAll(`article:not(.${selectedFilter})`); //find the elements on the site
+        let itemsToShow = document.querySelectorAll(`.${selectedFilter}`);
+
+        if (selectedFilter == 'all') { //if the user choose all
+        itemsToHide = [];
+        itemsToShow = document.querySelectorAll(`article`);
+        }
+        itemsToHide.forEach(el => { //if the user choose one, then hide everything else
+            el.classList.add('hide');
+            el.classList.remove('show');
+        });
+        itemsToShow.forEach(el => { //if the user choose one, then show it
+            el.classList.remove('hide');
+            el.classList.add('show'); 
+        });
+    });
+});
